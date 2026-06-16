0f08dee49edf0b7575ba8e6f84b848c3872d84ef
Mo.Omer <mmr42028@gmail.com>
2026-06-16 23:48:36 +0200

    fix aiven connection stable

--- db.php
+++ db.php
@@ -1,14 +1,17 @@
 <?php  
 
-$conn = mysqli_connect(
-    "mysql-31d9a205-coffeeshop.b.aivencloud.com",
-    "avnadmin",
-    "AVNS_ZHlShQkpcNtIERPZYVt",
-    "defaultdb",
-    13324
-);
+$host = "mysql-31d9a205-coffeeshop.b.aivencloud.com";
+$user = "avnadmin";
+$pass = "AVNS_ZHlShQkpcNtIERPZYVt";
+$db   = "defaultdb";
+$port = 13324;
 
-if (!$conn) {
+$conn = mysqli_init();
+
+/* مهم جداً لـ Aiven SSL */
+mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+
+if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port)) {
     die("Database connection failed: " . mysqli_connect_error());
 }
 
